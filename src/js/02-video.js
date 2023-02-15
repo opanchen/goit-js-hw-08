92f11c13091dafa4a8bec33f64b00b186d6c4497
opanchen <panchenkooleh@gmail.com>
2023-02-15 16:10:56 +0200

    task 2 demo done

---
+++ src/js/02-video.js
@@ -0,0 +1,27 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const storageData = localStorage.getItem('videoplayer-current-time');
+// console.log(storageData);
+const { seconds } = JSON.parse(storageData);
+// console.log(seconds);
+
+player.setCurrentTime(seconds);
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.on('pause', function () {
+  console.log('pause');
+});
+
+player.on('timeupdate', throttle(onTimeCount, 1000));
+
+function onTimeCount(currentTime) {
+  //   console.log(currentTime);
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(currentTime));
+}
